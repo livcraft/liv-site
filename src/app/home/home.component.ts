2d1b5e1dbd3916cd9d15157918bc698f15966e26
schirm <schirm@mit.edu>
2023-01-21 13:18:50 -0600

    carousel feature

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,64 +10,72 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    // Write Name
-    const letters = ['o','l','i','v','i','a',' ','s','c','h','i','r','m'];
+    
+    const text1_options = [
+      "turtles",
+      "hey",
+      "Is life actually real?",
+      "yuh",
+      "pog"
+    ];
+
+    const image_options = [
+      "../assets/spotthecow.jpeg",
+      "../assets/picoarcade.png",
+      "../assets/tiltydude.png",
+      "../assets/blerobot.jpg",
+      "../assets/fddr.png"
+    ];
     var i = 0;
-    var gitDone = 0;
-    var linkDone = 0;
-
-    function showPic() {
-      const pic = document.getElementById('pic');
-
-      if (pic) {
-        pic.innerHTML += "<img src=\"../assets/livpic.png\" width=\"200px\" height=\"200px\">";
-      }
-
-      setTimeout(about, 150);
-    }
-
-    function about() {
-      const abt = document.getElementById('about');
-
-      if (abt) {
-        abt.innerHTML += '<p>i\'m a senior studying <b>electrical engineering</b> and <b>computer science</b> with a history concentration at MIT</p> <p>i\’m passionate about microcontrollers, embedded systems, and creating cool projects<br>i love to talk all things computers, from upper-level software down to the chips that make it run</p> <p>outside of the classroom, i enjoy hiking, baking, watching thrillers, and traveling</p>'
-      }
-    }
-
-    function showQuestions() {
-      const git = document.getElementById('git');
-      const linkd = document.getElementById('linkedin');
-      //const prj = document.getElementById('projects-link');
-
-      if (git && gitDone == 0) {
-        git.innerHTML = "git";
-        gitDone = 1;
-        setTimeout(showQuestions, 150);
-      } else if (linkd && linkDone == 0) {
-        linkd.innerHTML = "linkedin";
-        linkDone = 1;
-        setTimeout(showPic, 150);
-      } //else if (prj) {
-       // prj.innerHTML = "projects";
-        //setTimeout(showPic, 150);
-      //}
-    }
-
-    function writeQuestion() {
-      const name = document.getElementById('name');
-      if (name) {
-        name.innerHTML += letters[i];
-  
-        if (i < 12) {
-          i++;
-          setTimeout(writeQuestion, 150);
-        } else {
-          setTimeout(showQuestions, 150);
-        }
-      } 
-    }
-
-    writeQuestion();
+    const currentOptionText1 = document.getElementById("current-option-text1");
+    const currentOptionImage = document.getElementById("image");
+    const carousel = document.getElementById("carousel-wrapper");
+    const mainMenu = document.getElementById("menu");
+    const optionPrevious = document.getElementById("previous-option");
+    const optionNext = document.getElementById("next-option");
+    
+    if (currentOptionText1) currentOptionText1.innerHTML = text1_options[i];
+    if (currentOptionImage) currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+    
+    if (optionNext) {
+    optionNext.onclick = function () {
+      i = i + 1;
+      i = i % text1_options.length;
+      if (currentOptionText1) currentOptionText1.dataset['nextText'] = text1_options[i];
+    
+      if (carousel) carousel.classList.add("anim-next");
+      
+      setTimeout(() => {
+        if (currentOptionImage) currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+      }, 455);
+      
+      setTimeout(() => {
+        if (currentOptionText1) currentOptionText1.innerHTML = text1_options[i];
+        if (carousel) carousel.classList.remove("anim-next");
+      }, 650);
+    };
   }
+    if (optionPrevious) {
+    optionPrevious.onclick = function () {
+      if (i === 0) {
+        i = text1_options.length;
+      }
+      i = i - 1;
+      if (currentOptionText1) currentOptionText1.dataset['previousText'] = text1_options[i];
+    
+      if (carousel) carousel.classList.add("anim-previous");
+    
+      setTimeout(() => {
+        if (currentOptionImage) currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+      }, 455);
+      
+      setTimeout(() => {
+        if (currentOptionText1) currentOptionText1.innerHTML = text1_options[i];
+        if (carousel) carousel.classList.remove("anim-previous");
+      }, 650);
+    };
+  }
+
+   }
 
 }
