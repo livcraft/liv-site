457fd2d3f68427ad25067160edda87172965d1c1
schirm <schirm@mit.edu>
2023-01-24 11:16:10 -0600

    updated projects

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,8 +12,20 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     
     const text1_options = [
-      "turtles",
-      "hey",
+      `spot the cow
+      
+      
+      a final project for computational design and fabrication focused on reducing time and material waste when 3D printing support points
+      
+      
+      with tree support structure, saw a 50% decrease in print time and 70.76% decrease in support material used`,
+      `pico arcade
+      
+      
+      designed a through-hole PCB board with Altium for a Raspberry Pi Pico arcade game and strategized routing layout with product design to maximize the player experience
+      
+      
+      developed game scripts in MicroPython, including two-player ping pong and memorization game`,
       `tilty dude
 
 
@@ -21,8 +33,20 @@ export class HomeComponent implements OnInit {
 
       
       wrote an I2C library for the MPU6050 accelerometer and implemented SPI communication for the TFT screen`,
-      "yuh",
-      "pog"
+      `bluetooth controlled car
+      
+
+      made during a seminar hosted by Infineon at MIT Jan. 2022 focused on playing with the PSoC 6
+      
+
+      first experience with BLE, used MIT's quick app service to create remote controller for the phone to drive car`,
+      `finger dance dance revolution
+      
+
+      a final project made by my small group in an Interconnected Embedded Systems class
+      
+
+      made using an ESP32 microcontroller, joystick shield, speaker, and LCD screen, with minimal HTML and CSS site coding, and loads of C++`
     ];
 
     const image_options = [
