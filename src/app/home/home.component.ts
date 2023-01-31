3517b136b9095b0b244d4e8fc1e5524fd984504e
schirm <schirm@mit.edu>
2023-01-31 13:23:49 -0600

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,41 +12,11 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     
     const text1_options = [
-      `spot the cow
-      
-      
-      a final project for computational design and fabrication focused on reducing time and material waste when 3D printing support points
-      
-      
-      with tree support structure, saw a 50% decrease in print time and 70.76% decrease in support material used`,
-      `pico arcade
-      
-      
-      designed a through-hole PCB board with Altium for a Raspberry Pi Pico arcade game and strategized routing layout with product design to maximize the player experience
-      
-      
-      developed game scripts in MicroPython, including two-player ping pong and memorization game`,
-      `tilty dude
-
-
-      designed and developed a tilting-based game with PSoC Creator powered by a PSoC 5LP stick
-
-      
-      wrote an I2C library for the MPU6050 accelerometer and implemented SPI communication for the TFT screen`,
-      `bluetooth controlled car
-      
-
-      made during a seminar hosted by Infineon at MIT Jan. 2022 focused on playing with the PSoC 6
-      
-
-      first experience with BLE, used MIT's quick app service to create remote controller for the phone to drive car`,
-      `finger dance dance revolution
-      
-
-      a final project made by my small group in an Interconnected Embedded Systems class
-      
-
-      made using an ESP32 microcontroller, joystick shield, speaker, and LCD screen, with minimal HTML and CSS site coding, and loads of C++`
+      "<u>spot the cow</u> <br> a final project for computational design and fabrication focused on reducing time and material waste when 3D printing support points <br> <br> with tree support structure, saw a 50% decrease in print time and 70.76% decrease in support material used",
+      "<u>pico arcade</u> <br> designed a through-hole PCB board with Altium for a Raspberry Pi Pico arcade game and strategized routing layout with product design to maximize the player experience <br> <br> developed game scripts in MicroPython, including two-player ping pong and memorization game",
+      "<u>tilty dude</u> <br> designed and developed a tilting-based game with PSoC Creator powered by a PSoC 5LP stick <br> <br> wrote an I2C library for the MPU6050 accelerometer and implemented SPI communication for the TFT screen",
+      "<u>bluetooth controlled car</u> <br> made during a seminar hosted by Infineon at MIT Jan. 2022 focused on playing with the PSoC 6 <br> <br> first experience with BLE, used MIT's quick app service to create remote controller for the phone to drive car",
+      "<u>finger dance dance revolution</u> <br> a final project made by my small group in an Interconnected Embedded Systems class <br><br> made using an ESP32 microcontroller, joystick shield, speaker, and LCD screen, with minimal HTML and CSS site coding, and loads of C++"
     ];
 
     const image_options = [
@@ -64,7 +34,7 @@ export class HomeComponent implements OnInit {
     const optionPrevious = document.getElementById("previous-option");
     const optionNext = document.getElementById("next-option");
     
-    if (currentOptionText1) currentOptionText1.innerText = text1_options[i];
+    if (currentOptionText1) currentOptionText1.innerHTML = text1_options[i];
     if (currentOptionImage) currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
     
     if (optionNext) {
@@ -80,7 +50,7 @@ export class HomeComponent implements OnInit {
       }, 455);
       
       setTimeout(() => {
-        if (currentOptionText1) currentOptionText1.innerText = text1_options[i];
+        if (currentOptionText1) currentOptionText1.innerHTML = text1_options[i];
         if (carousel) carousel.classList.remove("anim-next");
       }, 650);
     };
@@ -100,7 +70,7 @@ export class HomeComponent implements OnInit {
       }, 455);
       
       setTimeout(() => {
-        if (currentOptionText1) currentOptionText1.innerText = text1_options[i];
+        if (currentOptionText1) currentOptionText1.innerHTML = text1_options[i];
         if (carousel) carousel.classList.remove("anim-previous");
       }, 650);
     };
